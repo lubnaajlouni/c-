4fd339c4e3c927f749a4c25265583aa91ca32557
lubnaajlouni <115452696+lubnaajlouni@users.noreply.github.com>
2022-11-30 22:58:25 +0300

    Add files via upload

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label">number 1</asp:Label>
+            <br/>
+            <asp:TextBox ID="TextBox1" runat="server" ></asp:TextBox>
+            <br/>
+
+             <asp:Label ID="Label2" runat="server" Text="Label">number 2</asp:Label>
+            <br/>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br/>  <br/>
+
+            <asp:Button ID="Button2" runat="server" Text="-" OnClick="Button1_Click" />
+            <asp:Button ID="Button3" runat="server" Text="+" OnClick="Button2_Click" />
+            <asp:Button ID="Button4" runat="server" Text="*" OnClick="Button3_Click" />         
+            <asp:Button ID="Button5" runat="server" Text="/" OnClick="Button4_Click" /><br/> <br/>       
+
+
+               <asp:Label ID="Label3" runat="server" Text="Result"></asp:Label>
+               <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+
+            
+           
+
+
+
+
+
+
+
+
+            
+        </div>
+    </form>
+    <p>
+
+        &nbsp;</p>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+       
+
+      
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(TextBox1.Text);
+            double number2 = Convert.ToDouble(TextBox2.Text);
+            double s = number1 - number2;
+            TextBox3.Text = s.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(TextBox1.Text);
+            double number2 = Convert.ToDouble(TextBox2.Text);
+            double s = number1 + number2;
+            TextBox3.Text = s.ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(TextBox1.Text);
+            double number2 = Convert.ToDouble(TextBox2.Text);
+            double s = number1 * number2;
+            TextBox3.Text = s.ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            double number1 = Convert.ToDouble(TextBox1.Text);
+            double number2 = Convert.ToDouble(TextBox2.Text);
+            double s = number1 / number2;
+            TextBox3.Text = s.ToString();
+        }
+    }
+}
